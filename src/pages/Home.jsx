cecab5350c34e273d0adc88b5ca45f1c6c66ddb3
freekomaiyasuaki <free.komaiyasuaki@gmail.com>
2025-08-26 07:18:22 +0900

    add tailwindcss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,9 @@
-import Footer from "../components/Footer/Footer";
-import Header from "../components/Header/Header";
 import Main from "../components/Home/Main/Main";
-import styles from "./Home.module.css";
 
 export default function Home({ data }) {
   // console.log(data);
   return (
-    <div className={styles.home}>
+    <div>
       <Main data={data} />
     </div>
   );
