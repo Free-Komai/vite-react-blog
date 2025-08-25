cb16bbe65d6e41e6ec08e280bfdb166a8d78a4a9
freekomaiyasuaki <free.komaiyasuaki@gmail.com>
2025-08-26 07:45:03 +0900

    css

--- src/components/Home/Main/Main.jsx
+++ src/components/Home/Main/Main.jsx
@@ -1,17 +1,17 @@
-import styles from "./Main.module.css";
+// import styles from "./Main.module.css";
 import { HiDesktopComputer } from "react-icons/hi";
 
 export default function Main({ data }) {
   return (
-    <section className={styles.section}>
+    <section className="">
       <h1>Portfolio</h1>
-      <div className={styles.dataArea}>
+      <div className="flex gap-1">
         {data.map((data, num) => (
-          <dl key={num}>
-            <span>
+          <dl key={num} className="p-3 bg-green-600 mx-auto">
+            <div className="flex items-center justify-center">
               <HiDesktopComputer />
               <dt>{data.title}</dt>
-            </span>
+            </div>
             <dd>{data.description}</dd>
           </dl>
         ))}
