145549eca8d252d679ab05249fc5d8aad2d82fd9
freekomaiyasuaki <free.komaiyasuaki@gmail.com>
2025-08-28 07:56:39 +0900

    title refact

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
     <section className="relative bg-gradient-to-br from-gray-400">
       <Link to="/">
         <Icons />
-        <h1 className="absolute top-1/2 left-1/2  w-8/10 text-[8vw] -translate-1/2 text-center font-bold">
-          ポートフォリオBLOG
+        <h1 className="absolute top-1/2 left-1/2  w-8/10 text-[5vw] -translate-1/2 text-center font-bold">
+          Webコンテンツを紹介する
         </h1>
       </Link>
       <P5 />
