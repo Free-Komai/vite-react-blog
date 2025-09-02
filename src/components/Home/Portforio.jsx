486869d431705cfcd1b8ae15d1813a1f192415ad
freekomaiyasuaki <free.komaiyasuaki@gmail.com>
2025-09-02 21:49:44 +0900

    motion

--- src/components/Home/Portforio.jsx
+++ src/components/Home/Portforio.jsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line no-unused-vars
+import { motion } from "motion/react";
 import { HiDesktopComputer } from "react-icons/hi";
 import { Link } from "react-router-dom";
 
@@ -9,7 +11,17 @@ export default function Portforio({ portforioData }) {
       </h1>
       <div className="flex gap-[5%] flex-wrap">
         {portforioData.map((data, num) => (
-          <dl key={num} className="w-[47.5%] sm:w-[30%] p-3 hover:opacity-90">
+          <motion.dl
+            key={num}
+            initial={{ rotateX: 0 }}
+            whileInView={{ rotateX: 360 }}
+            viewport={{ once: 1 }}
+            transition={{
+              duration: 1,
+              delay: num * 0.3,
+            }}
+            className="w-[47.5%] sm:w-[30%] p-3 hover:opacity-90"
+          >
             <div className="flex items-center">
               <HiDesktopComputer />
               <dt>{data.title}</dt>
@@ -17,7 +29,7 @@ export default function Portforio({ portforioData }) {
             <Link to={`/contents/${num}`}>
               <img src={data.url} alt="testImage" />
             </Link>
-          </dl>
+          </motion.dl>
         ))}
       </div>
     </section>
--- src/components/Home/Skill.jsx
+++ src/components/Home/Skill.jsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line no-unused-vars
+import { motion } from "motion/react";
 export default function Skill({ skillData }) {
   return (
     <section className="container m-auto mt-10">
@@ -6,13 +8,23 @@ export default function Skill({ skillData }) {
       </h1>
       <div className="flex gap-[5%] flex-wrap">
         {skillData.map((data, num) => (
-          <dl key={num} className="w-30 p-3 ml-2">
+          <motion.dl
+            key={num}
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{
+              delay: num * 0.3,
+              type: "spring",
+            }}
+            className="w-30 p-3 ml-2"
+          >
             <div className="flex items-center">
               <dt className="font-bold">{data.name}</dt>
             </div>
             <dd className="p-1 pl-3">{data.star}</dd>
             <img src={data.image.url} alt="testImage" />
-          </dl>
+          </motion.dl>
         ))}
       </div>
     </section>
