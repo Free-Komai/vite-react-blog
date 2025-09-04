49e9e8c94ef672d714f64360da4978de3ce01f33
freekomaiyasuaki <free.komaiyasuaki@gmail.com>
2025-09-05 05:21:23 +0900

    Contents refact

--- src/pages/Contents.jsx
+++ src/pages/Contents.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import { motion } from "motion/react";
 import { useParams } from "react-router-dom";
+import Sub from "../components/Home/Sub";
 
 export default function Contents({ portforioData }) {
   window.scrollTo(0, 0);
@@ -10,16 +11,7 @@ export default function Contents({ portforioData }) {
     <>
       {portforioData[id] && (
         <div className="container m-auto p-4">
-          <h1 className="text-2xl font-bold mb-2">{portforioData[id].title}</h1>
-          <motion.div
-            className="bg-amber-400 w-[100%] h-1 bg-gradient-to-r from-amber-400 to-amber-50"
-            initial={{ x: "-90vw", opacity: 0 }}
-            whileInView={{ x: 0, opacity: 1 }}
-            // viewport={{ once: 1 }}
-            transition={{
-              duration: 1,
-            }}
-          />
+          <Sub title={portforioData[id].title} />
           <div className="p-1.5">{portforioData[id].description}</div>
           <img
             src={portforioData[id].url}
